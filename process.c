523bdfd3eb9d4efe417d0e07291976bdd553115b
william <wwwy3y3@gmail.com>
2014-01-18 21:41:28 +0800

    print process

--- process.c
+++ process.c
@@ -1,56 +1,23 @@
-#include <linux/module.h>
+#include <linux/kernel.h >
+#include <linux/sched.h >
+#include <linux/module.h >
 
-void printPid(){
-        DIR * dir;
-        DIR * dir2;
-        FILE *pidStat;
-    struct dirent * ptr;
-    dir =opendir("/proc");
-    //dir =opendir("./");
-    char cmds[32768][100]= {"\0"};
-    int i;
+int init_module(void)
+{
+	struct task_struct *task;
+	for_each_process(task)
+	{
+		printk("%s [%d]\n",task->comm , task->pid);
+	}
 
-    while((ptr = readdir(dir))!=NULL)
-    {
-            char pathname[100];
-        sprintf(pathname,"/proc/%s", ptr->d_name);
-        //sprintf(pathname,"./%s", ptr->d_name);
-        if((dir2 = opendir(pathname))!=NULL) {
-            //printf("%s: file\n", ptr->d_name);
-            int pid= strtol(ptr->d_name, NULL, 10);
-            if(pid != 0){ //a pid folder
-                    sprintf(pathname, "%s%s",pathname, "/stat");
-                    pidStat= fopen(pathname, "rb");
-                    char num[10];
-                    char proc[100];
+	return 0;
+}
 
-                    fscanf(pidStat, "%s", num);
-                    fscanf(pidStat, "%s", proc);
-                    strcpy(cmds[atoi(num)], proc);
-            }
-        }
-    }
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Cleaning Up.\n");
+}
 
-    printk("pid         name \n");
-    for (i = 0; i < 32768; ++i)
-    {
-            if(strcmp(cmds[i], "\0")!=0)
-                    printk("%d           %s\n",i, cmds[i]);
-    }
-    //free(cmds);
-    closedir(dir);
-} 
-
-int init_func(void) 
-{ 
- printPid();
- return 0; 
-} 
-
-void cleanup_func(void) 
-{ 
- printk("Goodbye world !\n"); 
-} 
-
-module_init(init_func); 
-module_exit(cleanup_func);
+MODULE_LICENSE("GPL");    
+module_init(init_module);
+module_exit(cleanup_module);
